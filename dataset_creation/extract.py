a38e285d0fcf104e6790c00f36675e5f23938dda
Alan Loovees <alanloovees@gmail.com>
2021-04-13 16:07:52 +0530

    will check for average positivity of tweets of a user now

--- dataset_creation/extract.py
+++ dataset_creation/extract.py
@@ -9,16 +9,21 @@ from textblob import TextBlob
 
 
 def getAccountInfo(username):
+
     print("\nAnalysing @" + username + "\n")
     hashtags = []
     mentions = []
     userMentions = 0
     tweet_count = 0
     tweetsToAnalyse = 1000
+    avgPositivity = 0
+
+
     end_date = datetime.utcnow() - timedelta(days=365)
     for status in tw.Cursor(api.user_timeline, id=username).items(tweetsToAnalyse):
         tweet_count += 1
         print("Number of tweets analysed = ", tweet_count, end="\r")
+        avgPositivity += checkPositivity(status.text)
         if hasattr(status, "entities"):
             entities = status.entities
         if "user_mentions" in entities:
@@ -33,13 +38,16 @@ def getAccountInfo(username):
         if status.created_at < end_date:
             break
 
-    print("\n\nUser mentions: " + str(userMentions))
+    avgPositivity /= tweetsToAnalyse
+    print("\n\nAverage positivity of tweets: " + str(avgPositivity))
+    print("\nUser mentions: " + str(userMentions))
     print("\nProcessed " + str(tweet_count) + " tweets from @" + username)
 
-    return userMentions
+    return userMentions, avgPositivity
 
 
 def checkPositivity(tweet):
+
     blob = TextBlob(tweet)
     for sentence in blob.sentences:
         return sentence.sentiment.polarity
@@ -47,6 +55,7 @@ def checkPositivity(tweet):
 
 # Authentication
 def apiAuth():
+
     accessToken = '1278165229004718081-62nBSL0dv4OU37xXTCltoz9Bk4sg1L'
     accessTokenSecret = 'UFaQpIgzs1cz7zxIB3TfEcopghUIAzg5uFyDobY4iuwbn'
     apiKey = 'NpZucVrr5mQcn8CPlvKU5N3vu' 
@@ -66,18 +75,19 @@ tweetsFetched = 0
 # Data extraction details
 searchWord = 'life' 
 dateSince = '2020-04-01'
-noOfTweets = 1
+noOfTweets = 2
 
 print("\nExtracting tweets....")
 
 tweets = tw.Cursor(api.search, q = 'searchWord -filter:retweets', lang ='en', since = dateSince).items(noOfTweets)
 
 # Format of the tweet and details in the CSV file
-tweetFormat = [["Geo","Tweet","Username","Location","Followees","Followers","No of posts", "User Mentions"]]
+tweetFormat = [["Geo", "Tweet", "Username", "Location", "Followees", "Followers", "No of posts", "User Mentions", "Avg Positivity", ""]]
 
 # Looping to find User Details and Data to be extracted
 for tweet in tweets:
-    tweetDetails.append([tweet.geo, tweet.text, tweet.user.screen_name, tweet.user.location, api.get_user(tweet.user.screen_name).friends_count, api.get_user(tweet.user.screen_name).followers_count, api.get_user(tweet.user.screen_name).statuses_count, getAccountInfo(tweet.user.screen_name)])
+    userMentions, avgPositivity = getAccountInfo(tweet.user.screen_name)
+    tweetDetails.append([tweet.geo, tweet.text, tweet.user.screen_name, tweet.user.location, api.get_user(tweet.user.screen_name).friends_count, api.get_user(tweet.user.screen_name).followers_count, api.get_user(tweet.user.screen_name).statuses_count, userMentions, avgPositivity])
 
 # Creating a CSV file of extracted data
 with open("tweets_100.csv", "w+") as file:
