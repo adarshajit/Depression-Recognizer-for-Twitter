545e286ff5052f6cf2583fe03eebc3bc9a8bbec1
Akshay <akshai7susanth@gmail.com>
2021-04-16 13:28:21 +0530

    Added code to remove user mentions and hyperlinks from tweets

--- dataset_creation/extract.py
+++ dataset_creation/extract.py
@@ -1,10 +1,7 @@
 import tweepy as tw
 import csv
-import json
-from datetime import datetime
-from datetime import timedelta
+import re
 from tweepy.auth import OAuthHandler
-
 
 def readwords( filename ):
     f = open(filename)
@@ -28,15 +25,16 @@ def posNeg(paragraph):
 		if w in negative:
 		    neg += 1
 	return pos,neg
-
+	
+def cleaning(tweet):
+	tweet = re.sub('@[\w]+','',tweet)
+	tweetCleaned = re.sub(r"http\S+", "", tweet)
+	return tweetCleaned
 
 # Counts the number of words in tweet
 def countWords(tweet):
 	tweetList = tweet.split(" ")
 	return(len(tweetList))
-
-
-
 
 # Authentication
 def apiAuth():
@@ -51,19 +49,17 @@ def apiAuth():
     api = tw.API(auth,wait_on_rate_limit=True)
     return api
 
-
 tweetDetails = [["Tweet", "Followees", "Followers", "No of posts", "Retweet count", "Favorite count", "Positive words", "Negative words", "No: of Words"]] # Format of the tweet and details in the CSV file
 api = apiAuth()
 
 # Data extraction details
-searchWord = 'joy' or 'happy' or 'family' or 'peace'
-dateSince = '2020-04-01'
+searchWord = 'motivation'
 noOfTweets = 10
 tweetStatus = 0
 
 print("\nExtracting tweets....\n")
 
-tweets = tw.Cursor(api.search, q = searchWord, lang ='en', exclude='retweets', since = dateSince).items(noOfTweets)
+tweets = tw.Cursor(api.search, q = searchWord, lang ='en', exclude='retweets').items(noOfTweets)
 
 # Looping to find User Details and Data to be extracted
 for tweet in tweets:
@@ -71,12 +67,11 @@ for tweet in tweets:
 	print("Progress: " + str(tweetStatus) + "/" + str(noOfTweets) + " tweets", end="\r")
 	pos, neg = posNeg(tweet.text)
 	countOfWords = countWords(tweet.text)
-	tweetDetails.append([tweet.text, api.get_user(tweet.user.screen_name).friends_count, api.get_user(tweet.user.screen_name).followers_count, api.get_user(tweet.user.screen_name).statuses_count, tweet.retweet_count, tweet.favorite_count, pos, neg,countOfWords])
+	
+	tweetDetails.append([cleaning(tweet.text), api.get_user(tweet.user.screen_name).friends_count, api.get_user(tweet.user.screen_name).followers_count, api.get_user(tweet.user.screen_name).statuses_count, tweet.retweet_count, tweet.favorite_count, pos, neg, countOfWords])
+	
+	# Creating a CSV file of extracted data
+	with open("tweets_test.csv", "w+") as file:
+		writer = csv.writer(file, delimiter=',')
+		writer.writerows(tweetDetails)
 
-# Creating a CSV file of extracted data
-with open("tweets_100.csv", "w+") as file:
-    print("\nWriting to CSV file...")
-    writer = csv.writer(file, delimiter=',')
-    writer.writerows(tweetDetails)
-
-print("\nProcess completed successfully!")
