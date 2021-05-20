c5c8dfe5436ee2610f9b44672df0639df27c20c1
Akshay <akshai7susanth@gmail.com>
2021-05-20 11:31:00 +0530

    Trained using user activity

---
+++ training/activityModel.py
@@ -0,0 +1,33 @@
+import csv
+import numpy as np
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support
+
+
+vectorizer = CountVectorizer()
+
+datasetReader = pd.read_csv('./cleanedDataset.csv')
+
+df = pd.DataFrame(datasetReader)
+
+allFeatures = vectorizer.fit_transform(datasetReader.Tweet)
+
+X = np.asarray(df[['Followees', 'Followers', 'No of posts', 'Retweet count', 'Favorite count', 'Positive words', 'Negative words', 'No: of Words']])
+Y = np.asarray(df['Depressed'])
+
+xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size = 0.3, random_state = 88)
+
+classifier = MultinomialNB()
+classifier.fit(xTrain, yTrain)
+
+nr_correct = (yTest == classifier.predict(xTest)).sum()
+nr_incorrect = yTest.size - nr_correct
+
+precision, recall, fscore, support = precision_recall_fscore_support(yTest, classifier.predict(xTest), average=None, labels=[0,2,4])
+
+print("Recall = ", recall, " Precision = ", precision, " & F1 Score = ", fscore)
