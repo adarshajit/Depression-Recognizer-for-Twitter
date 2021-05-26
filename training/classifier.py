8d454e798bc9c1548cda489edd3618e677360575
AkshaySusanth <akshai7susanth@gmail.com>
2021-05-26 18:44:42 +0530

    Combined all features

--- training/classifier.py
+++ training/classifier.py
@@ -5,41 +5,45 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn import metrics
+from sklearn.metrics import roc_curve, roc_auc_score
+import matplotlib.pyplot as plt
+from yellowbrick.classifier import ROCAUC
 
 vectorizer = CountVectorizer()
 
-datasetReader = pd.read_csv('./cleanedDataset.csv')
+datasetReader = pd.read_csv('./smallDataset.csv')
 
 df = pd.DataFrame(datasetReader)
 
 allFeatures = vectorizer.fit_transform(datasetReader.Tweet)
 
-X = np.asarray(df[['Followees', 'Followers', 'No of posts', 'Retweet count', 'Favorite count', 'Positive words', 'Negative words', 'No: of Words']])
-Y = datasetReader.Depressed
+A=allFeatures.toarray()
 
-#UserActivity model
-xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size = 0.3, random_state = 88)
+X = np.asarray(df[['Followees', 'Followers', 'No of posts', 'Retweet count', 'Favorite count', 'Positive words', 'Negative words', 'No: of Words']])
+
+
+X=np.array(X)
+A=np.array(A)
+
+allFeatures = np.hstack((A,X))
+
+Y=datasetReader.Depressed
+xTrain, xTest, yTrain, yTest = train_test_split(allFeatures, Y, test_size = 0.3, random_state = 88)
+
 classifier = MultinomialNB()
 classifier.fit(xTrain, yTrain)
 
-#DTM_Model
-xTrain1, xTest1, yTrain1, yTest1 = train_test_split(allFeatures, Y, test_size = 0.3, random_state = 88)
-classifier1 = MultinomialNB()
-classifier1.fit(xTrain1, yTrain1)
+yPred=classifier.predict(xTest)
+accuracy = metrics.accuracy_score(yTest, yPred)
 
-xsample = [xTest[0]] #Taking first sample of test dataset
-yProb=classifier.predict_proba(xsample) #Predicting probability of the first sample of test dataset
 
-print('Predicted Probabilities: ', yProb)
+print("accuracy:   %0.3f" % accuracy)
 
-#Normal prediction
-y_class = classifier.predict(xsample)
-y_class1 = classifier1.predict(xTest1)
 
-#Printing predicted class
-print('Predicted Class: ', y_class)
-print('Predicted Class1: ', y_class1[0])
 
-#Finding the index number to find which class has highest probability
-max_index_col = np.argmax(yProb[0], axis=0)
-print(max_index_col)
+print("Confusion Matrix:")
+print(metrics.confusion_matrix(yTest, yPred))
+
