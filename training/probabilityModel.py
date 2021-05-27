fa46c6a97c77a80fa43a7f7bd8a8173449cf1fb7
AkshaySusanth <akshai7susanth@gmail.com>
2021-05-27 12:46:40 +0530

    Training phase completed

--- training/probabilityModel.py
+++ training/probabilityModel.py
@@ -2,57 +2,37 @@ import pandas as pd
 import numpy as np
 import csv
 
-from sklearn.metrics import recall_score, precision_score, f1_score
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from yellowbrick.classifier import ROCAUC
 from sklearn import metrics
-
 
 vectorizer = CountVectorizer()
 
 datasetReader = pd.read_csv('./cleanedDataset.csv')
-# testdatasetReader = pd.read_csv('./testDataset.csv') #manually copy pasted data of 0, 2, and 4 separately
 
 df = pd.DataFrame(datasetReader)
-# dt = pd.DataFrame(testdatasetReader)
 
 allFeatures = vectorizer.fit_transform(datasetReader.Tweet)
-# testFeatures = vectorizer.transform(testdatasetReader.Tweet)
 
 xTrain, xTest, yTrain, yTest = train_test_split(allFeatures, datasetReader.Depressed, test_size = 0.3, random_state = 88)
 
 classifier = MultinomialNB()
 classifier.fit(xTrain, yTrain)
 
-# visualizer = ROCAUC(classifier, encoder={0: 'Non Depressed', 2: 'Mildly Depressed', 4: 'Higly Depressed'})
-
-# visualizer.fit(xTrain, yTrain)    
-# visualizer.score(xTest, yTest)        
-# visualizer.show()    
-
-# print(xTest)
-
-# print(test)
-
-yPred1=classifier.predict_proba(xTest) # Probability values of classifier with tweet only
-
+yPred1=classifier.predict_proba(xTest) 
 
 X = np.asarray(df[['Followees', 'Followers', 'No of posts', 'Retweet count', 'Favorite count', 'Positive words', 'Negative words', 'No: of Words']])
 Y = np.asarray(df['Depressed'])
-
-# test = np.asarray(dt[['Followees', 'Followers', 'No of posts', 'Retweet count', 'Favorite count', 'Positive words', 'Negative words', 'No: of Words']])
 
 xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size = 0.3, random_state = 88)
 
 classifier2 = MultinomialNB()
 classifier2.fit(xTrain, yTrain)
 
-yPred2=classifier2.predict_proba(xTest) # Probability values of classifier with activities only
+yPred2=classifier2.predict_proba(xTest) 
 
 pos = []
-
 weight = 0.1
 
 # Logarithmic addition for combining probabilities
@@ -76,22 +56,11 @@ for i in pos:
         else:
             tally.append(4)
 
-correct = 0
+print(metrics.classification_report(yTest, tally, target_names=['Not Depressed', 'Mildy Depressed', 'HighlyDepressed']))
 
-# Manually checking for TN and TP
+print("Confusion Matrix:")
+print(metrics.confusion_matrix(yTest, tally))
 
-for i,value in enumerate(yTest):
-    if value == tally[i]:
-        correct += 1
-
-accuracy = correct/len(yTest)
-
-print("Accuracy = ", accuracy)
-
-# accuracy = metrics.accuracy_score(yTest, yPred)
-# print("accuracy:   %0.3f" % accuracy)
-
-# print(metrics.classification_report(yTest, yPred, target_names=['ND', 'D', 'HD']))
-
-# print("Confusion Matrix:")
-# print(metrics.confusion_matrix(yTest, yPred))
+accuracy = metrics.accuracy_score(yTest, tally)
+    
+print("Accuracy = ", accuracy*100,"%")
