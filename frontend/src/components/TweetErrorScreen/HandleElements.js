c0d22783686cf992b06b60cd049eb5bc54797207
Alan Loovees <alanloovees@gmail.com>
2021-06-12 23:12:03 +0530

    added error screen for tweet not found

---
+++ frontend/src/components/TweetErrorScreen/HandleElements.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  width: 150px;
+  height: 46px;
+  background-color: #5991e6;
+  border-radius: 20px;
+  border-style: none;
+  color: #fff;
+  cursor: pointer;
+  font-weight: 600;
+`;
